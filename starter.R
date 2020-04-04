acfe2e16e677f15c0f15d4d0c3bace76106c566c
gomesfellipe <gomes.fellipe@hotmail.com>
2020-04-04 19:12:49 -0300

    new ideias

--- starter.R
+++ starter.R
@@ -5,12 +5,11 @@
 # todo: n de maiusculas
 # todo: % do texto que foi coletado
 # todo: usar busca de palavras cm sentimento negativo e retornar trexos com essas frases
-
+# todo: usar a palavra que vem antes e que vem depoois (3 submissoes)
+# todo: montar baseline para modelo de ml fazendo uma selecao aleatoria entre as ngrams 
+# (incluir regra para quando for neutro e realizar um teste depois para saber se a pena usar o neutro)
+# todo: calcular kappa para saber se o vader esta acertando como agnt espera
 # todo: comferir se precisa melhorar funcao de limpeza (one grams mais frequentes)
-
-# functions -----------------------------------------------------------------------------------
-
-
 
 # Script --------------------------------------------------------------------------------------
 
@@ -22,13 +21,13 @@ library(stringr)
 library(quanteda)
 
 train_data <- read_csv("data/train.csv")
-# test_data <- read_csv("data/train.csv")
+test_data <- read_csv("data/test.csv")
 
 # failures source: https://www.kaggle.com/c/tweet-sentiment-extraction/discussion/138272
 source("functions.R")
 
 # get metadata:
-# train_metadata <- get_metadata(train_data) # aguarde um pouco..
+train_metadata <- get_metadata(train_data) # aguarde um pouco..
 # test_metadata <- get_metadata(test_data) # aguarde um pouco..
 
 
@@ -43,8 +42,6 @@ train_data <-
   mutate_at(c("text", "selected_text"), ~ .x %>%
     str_trim() %>%
     str_squish()) # segundo as regras nao faz diferenca
-
-
 
 vader::getVader(train_data$text[1])
 
@@ -91,6 +88,7 @@ make_dataset <- function(x) {
   text_vader = x$text %>% map_chr(vader_compound)
   
   tibble(
+    txt_id = x$textID,
     txt = x$text,
     txt_len = str_split(x$text, pattern = " ", )[[1]] %>% length(),
     text_sentiment = x$sentiment,
@@ -107,11 +105,17 @@ make_dataset <- function(x) {
       dif_ngram_vader = map_chr(dif_ngram_vader, vader_compound)
     ) %>%
     rowwise() %>%
-    mutate(jaccard = jaccard(x$text, all_ngrams)) %>%
+    mutate(jaccard = jaccard(x$text, all_ngrams)) %>% # y
     ungroup()
 }
 
-results <- make_dataset(non_neutral[ind_max, ])
+
+# para a amostra
+results <- make_dataset(x = non_neutral[ind_max, ])
+# para todos nao neutros:
+results_nonneutral <- parallel::mclapply(non_neutral, make_dataset, mc.cores = 4)
+
+lexicon::hash_internet_slang
 
 
 
