5cefed43d3f8985d048d7bc9c6158d0bcc9981ad
gomesfellipe <gomes.fellipe@hotmail.com>
2020-04-05 19:07:03 -0300

    att complete parsed + pp + join with metadata

--- starter.R
+++ starter.R
@@ -31,48 +31,82 @@ source("functions.R")
 # get metadata:
 train_metadata <- get_metadata(train_data) # aguarde um pouco..
 # test_metadata <- get_metadata(test_data) # aguarde um pouco..
+# 
+# pre process 1 ---
+# train_pp <-
+#   train_data %>%
+#   mutate_at(c("text", "selected_text"), ~ .x %>%
+#     str_trim() %>% str_squish()) %>%  # segundo as regras nao faz diferenca
+#   filter(!is.na(text)) %>%
+#   filter(sentiment != "neutral") # Amostrar um tweet que nao seja neutro
+# 
+# glue::glue("Non-neutral are {round((nrow(train_pp)*100) / nrow(train_data),2)}% of full dataset")
+# 
+# nested_train_pp <- 
+#   train_pp %>% 
+#   # .[1:1000,] %>% 
+#   group_by(textID) %>% 
+#   tidyr::nest() 
+# 
+# t0 <- Sys.time()
+# train_parsed <-  
+#   foreach(x=nested_train_pp$data,
+#           y=nested_train_pp$textID,
+#           n=1:length(nested_train_pp$textID),
+#           # .export = ls(),
+#           # .packages = c(dplyr, purrr, stringr),
+#           # .errorhandling = c("pass"),
+#           .combine = rbind,
+#           .inorder = F
+#   ) %dopar% {
+#     tryCatch({
+#       cat(paste0("Parsing tweet ",n,": ", x$text," (", x$sentiment, ") \n"))
+#       make_dataset(x, y)
+#     }, error = function(e){print(paste0("Error in tweet: ", y, "\n", 
+#                                   "caused the error: '", e, "'\n")); NA})
+#   
+#   }
+# Sys.time() - t0
+# stopCluster(cl)
 
-# pre process 1
-train_pp <-
-  train_data %>%
-  mutate_at(c("text", "selected_text"), ~ .x %>%
-    str_trim() %>% str_squish()) %>%  # segundo as regras nao faz diferenca
-  filter(!is.na(text)) %>%
-  filter(sentiment != "neutral") # Amostrar um tweet que nao seja neutro
+# saveRDS(train_parsed, "data/train_parsed.rds")
 
-glue::glue("Non-neutral are {round((nrow(train_pp)*100) / nrow(train_data),2)}% of full dataset")
-
-nested_train_pp <- 
-  train_pp %>% 
-  # .[1:1000,] %>% 
-  group_by(textID) %>% 
-  tidyr::nest() 
-
-t0 <- Sys.time()
-train_parsed <-  
-  foreach(x=nested_train_pp$data,
-          y=nested_train_pp$textID,
-          n=1:length(nested_train_pp$textID),
-          # .export = ls(),
-          # .packages = c(dplyr, purrr, stringr),
-          # .errorhandling = c("pass"),
-          .combine = rbind,
-          .inorder = F
-  ) %dopar% {
-    tryCatch({
-      cat(paste0("Parsing tweet ",n,": ", x$text," (", x$sentiment, ") \n"))
-      make_dataset(x, y)
-    }, error = function(e){print(paste0("Error in tweet: ", y, "\n", 
-                                  "caused the error: '", e, "'\n")); NA})
-  
-  }
-Sys.time() - t0
-stopCluster(cl)
-
-saveRDS(train_parsed, "data/train_parsed.rds")
+# preproces2 ----
 train_parsed <- readRDS("data/train_parsed.rds")
 
+# remover linhas onde all_ngram eh igual do texto selecionado
+# porem o jaccard nao é igual a 1 para nao 'confundir' o algoritmo
+ind <- train_parsed %>% 
+  filter(all_ngrams == sel_text & jaccard != 1) %>% 
+  pull(textID) %>% unique()
 
+# aplicar filtro
+train_parsed %>% 
+  filter(ngram_vader == text_sentiment)
+
+# remover linhas aonde vader nao bate com o sentimento verdadeiro da frase
+train_parsed <- 
+  train_parsed %>% 
+  filter(!textID %in% ind) %>% 
+  filter(ngram_vader == text_sentiment)
+
+# combinar resultado com metadados e 
+# selecionar colunas interessantes para modelagem
+train_parsed <- 
+  train_parsed %>% 
+  left_join(train_metadata, by = c("textID", "text", "sel_text", "jaccard")) %>% 
+  select(textID, text, sel_text, all_ngrams, text_n_words, ngram_len, ngram_prop, ngram_vader, 
+         dif_text_ngram, dif_ngram_vader,text_nunique_words, text_len:text_atpeople, jaccard)
+
+# preencher metadados para todas as ngrams
+train_parsed <- 
+  train_parsed %>% 
+  # group_by(textID) %>% 
+  # arrange(-ngram_len) %>% 
+  fill(text_nunique_words:text_atpeople,.direction = "up")
+  # ungroup()
+  
+View(train_parsed)
 # Rascunho ------------------------------------------------------------------------------------
 
 # Mantenha o rascunho daqui para baixo
